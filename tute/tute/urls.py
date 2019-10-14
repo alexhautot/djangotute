d53a352da614125d13f6c45145cb07693aa105b0
a.hautot <alexander.vi@gmail.com>
2019-10-14 20:21:14 +1100

    project completed

--- tute/tute/urls.py
+++ tute/tute/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    
+
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("blog/", include("blog.urls")),
 
 ]
